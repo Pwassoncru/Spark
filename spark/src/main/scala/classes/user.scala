71ebe0d861c1cc63ed78bce16f494e81f0b02031
NemAmbulant <ksiezakhelene@yahoo.com.sg>
2018-07-07 18:59:15 +0200

    user

--- spark/src/main/scala/classes/user.scala
+++ spark/src/main/scala/classes/user.scala
@@ -1,1 +1,9 @@
-case class User(id : String, image : URI, nickname : String, verified : Boolean, deleted : Boolean)
+import java.time.Instant
+import java.net.URI
+
+case class User(id : String, updatedOn : Instant, image : URI, nickname : String, verified : Boolean, deleted : Boolean)
+
+object User
+{
+
+}
