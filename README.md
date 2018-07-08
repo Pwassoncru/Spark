ab51037bf13140909fcf9a548bfcee8e596b365a
Florian Lapeyre <lapeyr_a@epita.fr>
2018-07-08 07:30:02 +0200

    c3 done, but code is commented due to dependency problem

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
 # Spark
-Social network in scala/kafka
+Social network Back in scala/kafka
+
+In c4/, you'll find a tool anylising the data stored in
+> c4/data/posts/part-\*
+This tool ask you for a brand, then a duration (from 0 days to x months),
+and it gives you how many times the brand occured during this duration (from
+now).
+
+In spark/, you'll find an api of a social network, with a little sequential
+example showing how you can use this api.
+
+Make sure zookeeper and some servers are started, listening to the port 9092.
+Make also sure the topics "posts", "users", "messages", "sinkPosts", "sinkUsers"
+and "sinkMessages" are created.
 
 ## Options
 
+No bonus has been done
+
 ## Dependencies
+
+We used Kafka Streaming + Ktables, sending messages in csv, using the kafka
+String serializer. We also tried to use spark-streaming to convert the sink
+topics into hdfs, but we had a dependency problem that we didn't manae to solve
+(jackson dependency version), thus this feature isn't working.
+
+For the analistic tool, we used spark's RDD.
---
+++ spark/src/main/scala/writing.scala
@@ -0,0 +1,35 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.kafka010._
+import org.apache.kafka.common.serialization.StringDeserializer
+
+import PostUtils._
+import MessageUtils._
+import UserUtils._
+
+object RDDStreams {
+
+  /*println("RDD streams are starting")
+
+  val conf = new SparkConf().setAppName("redirectingKafkaStream").setMaster("local[*]")
+  val sc = new StreamingContext(conf, Seconds(5))
+
+  val config = Map[String,Object](
+    "bootstrap.servers" -> "localhost:9092",
+    "key.deserializer" -> classOf[StringDeserializer],
+    "value.deserializer" -> classOf[StringDeserializer],
+    "group.id" -> "use_a_separate_id_for_each_stream"
+  )
+
+  // See how it writes to hdfs => Maybe do 3 streams.
+  val topics = List("sinkPosts", "sinkMessages", "sinkUsers")
+
+  val stream = KafkaUtils.createDirectStream[String,String](
+    sc,
+    LocationStrategies.PreferConsistent,
+    ConsumerStrategies.Subscribe[String,String](topics, config))
+
+  stream.window(Minutes(30)).saveAsTextFiles("saved")
+
+  sc.start()*/
+}
